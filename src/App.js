03ab2cd806cc136820669619c2b8f9c328ee7dee
RianIslam <rianislamdr@gmail.com>
2021-01-24 23:55:41 +0600

    add some code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
-import React from 'react';
-import { useState } from 'react';
-import './App.css';
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  Input,
+  InputLabel,
+} from "@material-ui/core";
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [input,setInput] = useState('');
-  const [messages,setMessages] = useState(["geeki","hdfhd","dhfdf"]);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState(["geeki", "hdfhd", "dhfdf"]);
 
   console.log(input);
   console.log(messages);
 
-  const sendMessage = (event) =>{
-    setMessages ([...messages,input]);
-  setInput('')
-  }
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput("");
+  };
   return (
     <div className="App">
-      <input value={input} onChange={event =>setInput(event.target.value)}/>
-      <button onClick={sendMessage}>Send Message</button>
-    
-    {
-      messages.map(messages => (
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message</InputLabel>
+          <input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </Button>
+        </FormControl>
+      </form>
+      {messages.map((messages) => (
         <p>{messages}</p>
-      ))
-    }
+      ))}
     </div>
   );
 }
