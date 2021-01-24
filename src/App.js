090c691d3012c8a3c7c35e79aa3144424275ba3b
RianIslam <rianislamdr@gmail.com>
2021-01-24 17:50:19 +0600

    input add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState('');
+
+  console.log(input);
+
+  const sendMessage = (event) =>{
+    
+  }
   return (
     <div className="App">
-      <h1>hello all</h1>
+      <input value={input} onChange={event =>setInput(event.target.value)}/>
+      <button>Send Message</button>
     </div>
   );
 }
