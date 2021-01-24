a1f9f402fef00106158b6b47accf23166b507282
RianIslam <rianislamdr@gmail.com>
2021-01-24 19:17:53 +0600

    add message

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import './App.css';
 
 function App() {
   const [input,setInput] = useState('');
+  const [messages,setMessages] = useState(["geeki","hdfhd","dhfdf"]);
 
   console.log(input);
+  console.log(messages);
 
   const sendMessage = (event) =>{
-    
+    setMessages ([...messages,input]);
+  setInput('')
   }
   return (
     <div className="App">
       <input value={input} onChange={event =>setInput(event.target.value)}/>
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
+    
+    {
+      messages.map(messages => (
+        <p>{messages}</p>
+      ))
+    }
     </div>
   );
 }
