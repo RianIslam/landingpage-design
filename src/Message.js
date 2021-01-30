323c373709cbabe21c1bf575f9ba4694b8d51930
RianIslam <rianislamdr@gmail.com>
2021-01-30 20:43:51 +0600

    left chat

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,26 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
 import React from 'react'
+import './Message.css'
+const Message = ({message,username}) => {
 
-const Message = (props) => {
+    const isUser = username === message.username;
+
     return (
         <div>
-            <h2>{props.username} : {props.text}</h2>
+
+            <Card className={`message ${isUser && 'message_user'}`}>
+            <CardContent>
+                <Typography
+                    color="white"
+                    variant="h5"
+                    component="h2"
+                    >
+                    {message.username} : {message.text}
+                </Typography>
+            </CardContent>
+                
+            </Card>
+            
         </div>
     )
 }
